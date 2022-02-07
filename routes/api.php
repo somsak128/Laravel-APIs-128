994ccf156659cf8c8c57da822f108f7546a2a63e
Somsak128 <622021128@tsu.ac.th>
2022-02-07 21:49:31 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
