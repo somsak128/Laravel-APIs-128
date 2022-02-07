68023655510939ec093e54d9f9c2f08b9f449a15
Somsak128 <622021128@tsu.ac.th>
2022-02-07 20:42:28 +0700

    Update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UsersSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
